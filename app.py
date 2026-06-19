0053e9dff6134c054161855c8f71cee116b5c973
aadityendragauttam <aadityendragauttam@gmail.com>
2026-06-19 21:38:38 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request , url_for , render_template
+import joblib
+model = joblib.load(r'C:\Users\user\OneDrive\Desktop\Data Science\Loan-Approval-Detection-System\model\Loan_model.lb')
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -12,20 +14,25 @@ def contact():
 @app.route('/project')
 def project():
     return render_template('project.html')
+
 @app.route('/history')
 def history():
     return render_template('history.html')
 @app.route('/project',methods=['GET','POST'])
 def predict():
     if request.method == 'POST':
-        brand_name =request.form['brand_name']
-        owner =int(request.form['owner'])
-        age =int(request.form['age'])
-        power =int(request.form['power'])
-        kms_driven =int(request.form['kms_driven'])
-        print('Outcome :- ', [brand_name,owner,age,power,kms_driven])
+        income =int(request.form['income'])
+        credit_score =int(request.form['credit_score'])
+        loan_amount =int(request.form['loan_amount'])
+        years_employed =int(request.form['years_employed'])
+        print('Outcome :- ', [income,credit_score,loan_amount,years_employed])
+        pred = model.predict([[income,credit_score,loan_amount,years_employed]])
+        if pred == 0:
+            final = 'Not Approved'
+        else:
+            final = 'Approved'
     
-    return render_template('project.html')
+    return render_template('project.html',prediction = final)
 
 if __name__ == '__main__':
     app.run(debug=True)
